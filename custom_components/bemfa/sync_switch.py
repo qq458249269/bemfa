64f95f4ff353265719d5f77982cf65ebd4670ccd
yinxuehao <1@qq.com>
2026-07-31 15:30:55 +0800

    更新版本号至 1.4.0.3，并修复相机和锁状态的引用逻辑

--- custom_components/bemfa/sync_switch.py
+++ custom_components/bemfa/sync_switch.py
@@ -4,11 +4,14 @@ from __future__ import annotations
 from collections.abc import Mapping, Callable
 from typing import Any
 from homeassistant.components.automation import DOMAIN as AUTOMATION_DOMAIN
-from homeassistant.components.camera import DOMAIN as CAMERA_DOMAIN, STATE_IDLE
+from homeassistant.components.camera import (
+    CameraState,
+    DOMAIN as CAMERA_DOMAIN,
+)
 from homeassistant.components.group import DOMAIN as GROUP_DOMAIN
 from homeassistant.components.humidifier import DOMAIN as HUMIDIFIER_DOMAIN
 from homeassistant.components.input_boolean import DOMAIN as INPUT_BOOLEAN_DOMAIN
-from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN
+from homeassistant.components.lock import DOMAIN as LOCK_DOMAIN, LockState
 from homeassistant.components.media_player import DOMAIN as MEDIA_PLAYER_DOMAIN
 from homeassistant.components.remote import DOMAIN as REMOTE_DOMAIN
 from homeassistant.components.scene import DOMAIN as SCENE_DOMAIN
@@ -120,7 +123,7 @@ class Camera(Switch):
     def _msg_generator(
         self,
     ) -> Callable[[str, ReadOnlyDict[Mapping[str, Any]]], str | int]:
-        return lambda state, attributes: MSG_OFF if state == STATE_IDLE else MSG_ON
+        return lambda state, attributes: MSG_OFF if state == CameraState.IDLE else MSG_ON
 
 
 @SYNC_TYPES.register("media_player")
@@ -148,7 +151,9 @@ class Lock(Switch):
     def _msg_generator(
         self,
     ) -> Callable[[str, ReadOnlyDict[Mapping[str, Any]]], str | int]:
-        return lambda state, attributes: MSG_OFF if state == STATE_LOCKED else MSG_ON
+        return lambda state, attributes: (
+            MSG_OFF if state == LockState.LOCKED else MSG_ON
+        )
 
     def _service_names(self) -> tuple[str, str]:
         return (SERVICE_UNLOCK, SERVICE_LOCK)
